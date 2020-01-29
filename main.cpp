67d60c802d990e7e5df47c6e3d3ff6f3d386d0d2
Zain Ahmed <48980361+zainahmed0597@users.noreply.github.com>
2020-01-29 17:25:02 +0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num = 0;
+    while (num <= 20){
+        cout << "Hello world!" << num << endl;
+        num = num + 1;
+    }
+    return 0;
+}
